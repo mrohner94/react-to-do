3cc21d045b06c18435d5aa272b76f9c66508ccc8
mrohner94 <87048324+mrohner94@users.noreply.github.com>
2021-12-18 00:07:21 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import { useState } from 'react'
+
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App () {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Food Shopping',
+      day: 'Dec 5th at 2:30pm',
+      reminder: false
+    },
+    {
+      id: 2,
+      text: 'Christmas Gifts',
+      day: 'Dec 25th at 10:30am',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Office Shopping',
+      day: 'Dec 28th at 6:30pm',
+      reminder: false
+    },
+    {
+      id: 4,
+      text: 'Dinner with Friends',
+      day: 'Feb 2nd at 7:45pm',
+      reminder: true
+    }
+  ])
+
   return (
     <div className='container'>
       <Header title='To Do List' />
+      <Tasks tasks={tasks} />
     </div>
   )
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 
-function Button ({ name, onClick, color }) {
+const Button = ({ color, name, onClick }) => {
   return (
     <button
       className='btn'
